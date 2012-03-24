66022b7be23b6774caedc3ddadfa48d9e27f33d6
chaz <millerchaz@gmail.com>
2012-03-24 13:27:27 -0500

    pwm.c is working. I would like to get ustep to work, at least within pwm.c.

--- testing/pwm.c
+++ testing/pwm.c
@@ -1,9 +1,11 @@
 #define F_CPU 16000000UL //*16MHz*
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/pgmspace.h>
 
 
 void delay(uint16_t);
+void ustep(uint8_t);
 
 int main(void){
 
@@ -38,26 +40,93 @@ set.    |--0---|--0---|--0---|--0---|--0--|--0--|--1--|--0--|*/
 	PORTC = 0xFF;
 	PORTD = 0xFF;
 
+
 	while (1){
 	    static uint8_t dc; 
 	    dc++;
 	    OCR0A = dc;
-	    OCR0B = dc;
-	    OCR2A = dc;
-	    OCR2B = dc;
+	    OCR0B = dc+64;
+	    OCR2A = dc+128;
+	    OCR2B = dc+192;
+	    delay(5);
+
+	}
+    }//main
+    
+
+void ustep(uint8_t me){
+    /********************************************************
+    Sinewave output routine for unipolar stepper motor
+    1 is clockwise and 2 is anticlockwise 
+    *********************************************************/
+    static const uint8_t sinewave[32] PROGMEM = {0, 25, 50, 74, 98, 120, 142,
+    162,180,197, 212, 225, 236, 244, 250, 254, 255, 254, 250, 244, 236, 225,
+    212, 197, 180, 162, 142, 120, 98, 74, 50, 25};
+
+    static uint8_t state;
+    //I use the least significant nybble of byte 'state' to track PWM duty
+    //cycle, and the most significant nybble to change output pin state
+    //table every 90 degrees of the sinewave
+
+    //increment the duty cycles to microstep the motor
+    if(2==me){
+	state--;
+    }else if(1==me){
+	state++;
+    }
+
+    //write out the PWM compare register values
+   switch ((state >> 4)&3){ //X%N = X&(N-1) if X and N are powers of 2
+        case 0:
+            OCR0A = sinewave[(state+16)&31];
+            OCR0B = sinewave[state&31];
+            OCR2A = 0;
+            OCR2B = 0;
+            PORTB ^= (1 << 5);
+            break;
+        case 1:
+            OCR0A = 0;
+            OCR0B = sinewave[state&31];
+            OCR2A = sinewave[(state+16)&31];
+            OCR2B = 0;
+            PORTB ^= (1 << 5);
+            break;
+        case 2:
+            OCR0A = 0;
+            OCR0B = 0;
+            OCR2A = sinewave[(state+16)&31];
+            OCR2B = sinewave[state&31];
+            PORTB ^= (1 << 5);
+            break;
+        case 3:
+            OCR0A = sinewave[(state+16)&31];
+            OCR0B = 0;
+            OCR2A = 0;
+            OCR2B = sinewave[state&31];
+            PORTB ^= (1 << 5);
+            break;
+	}
+}//ustep
+/*
+	while (1){
+	    static uint8_t dc; 
+	    dc++;
+	    OCR0A = dc;
+	    OCR0B = dc+64;
+	    OCR2A = dc+128;
+	    OCR2B = dc+192;
 	    delay(5);
 
 	}
 
     for(;;){
-    delay(1);
+   delay(1);
     }
 }//main
-
+*/
 void delay(uint16_t me){
     while(me){
         _delay_ms(1);
         me--;
     }
 }
-
